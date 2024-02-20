ef7184670163b0de95f63e6eb594b73cae50686d
RishikantSri <rishi.kant.sri@gmail.com>
2024-02-20 17:32:40 +0530

    3d navigation button added

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'button.dart';
+import 'buttontapped.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -20,74 +21,120 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  bool darkMode = false;
+
+  // SET ICONS HERE
+
+  var icons1 = Icons.home;
+  var icons2 = Icons.settings;
+  var icons3 = Icons.favorite;
+  var icons4 = Icons.message;
+
+  //
+
+  bool buttonPressed1 = false;
+  bool buttonPressed2 = false;
+  bool buttonPressed3 = false;
+  bool buttonPressed4 = false;
+
+  void _letsPress1() {
+    setState(() {
+      buttonPressed1 = true;
+      buttonPressed2 = false;
+      buttonPressed3 = false;
+      buttonPressed4 = false;
+    });
+  }
+
+  void _letsPress2() {
+    setState(() {
+      buttonPressed1 = false;
+      buttonPressed2 = true;
+      buttonPressed3 = false;
+      buttonPressed4 = false;
+    });
+  }
+
+  void _letsPress3() {
+    setState(() {
+      buttonPressed1 = false;
+      buttonPressed2 = false;
+      buttonPressed3 = true;
+      buttonPressed4 = false;
+    });
+  }
+
+  void _letsPress4() {
+    setState(() {
+      buttonPressed1 = false;
+      buttonPressed2 = false;
+      buttonPressed3 = false;
+      buttonPressed4 = true;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: darkMode ? Colors.grey[850]! : Colors.grey[300]!,
+      backgroundColor: Colors.grey[300],
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.end,
           children: <Widget>[
-            Container(
-              width: 200,
-              height: 200,
-              child: Icon(Icons.star, size: 80, color: darkMode ? Colors.white : Colors.black),
-              decoration: BoxDecoration(
-                color: darkMode ? Colors.grey[850]! : Colors.grey[300]!,
-                borderRadius: BorderRadius.all(Radius.circular(50)),
-                boxShadow: [
-                  BoxShadow(
-                    color: darkMode ? Colors.black54 : Colors.grey[500]!,
-                    offset: Offset(4.0, 4.0),
-                    blurRadius: 15.0,
-                    spreadRadius: 1.0,
+            Padding(
+              padding: EdgeInsets.all(20),
+              child: Row(
+                children: <Widget>[
+                  Expanded(
+                    child: GestureDetector(
+                        // FIRST BUTTON
+                        onTap: _letsPress1,
+                        child: buttonPressed1
+                            ? ButtonTapped(
+                                icon: icons1,
+                              )
+                            : MyButton(
+                                icon: icons1,
+                              )),
                   ),
-                  BoxShadow(
-                    color: darkMode ? Colors.grey[800]! : Colors.white,
-                    offset: Offset(-4.0, -4.0),
-                    blurRadius: 15.0,
-                    spreadRadius: 1.0,
+                  Expanded(
+                    child: GestureDetector(
+                        // SECOND BUTTON
+                        onTap: _letsPress2,
+                        child: buttonPressed2
+                            ? ButtonTapped(
+                                icon: icons2,
+                              )
+                            : MyButton(
+                                icon: icons2,
+                              )),
+                  ),
+                  Expanded(
+                    child: GestureDetector(
+                        // THIRD BUTTON
+                        onTap: _letsPress3,
+                        child: buttonPressed3
+                            ? ButtonTapped(
+                                icon: icons3,
+                              )
+                            : MyButton(
+                                icon: icons3,
+                              )),
+                  ),
+                  Expanded(
+                    child: GestureDetector(
+                        // FOURTH BUTTON
+                        onTap: _letsPress4,
+                        child: buttonPressed4
+                            ? ButtonTapped(
+                                icon: icons4,
+                              )
+                            : MyButton(
+                                icon: icons4,
+                              )),
                   ),
                 ],
               ),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Padding(
-                  padding: EdgeInsets.only(top: 50, right: 3),
-                  child: ElevatedButton(
-                    style: ButtonStyle(
-                      backgroundColor: MaterialStateProperty.all<Color>(Colors.white),
-                      foregroundColor: MaterialStateProperty.all<Color>(Colors.black),
-                    ),
-                    onPressed: () {
-                      setState(() {
-                        darkMode = false;
-                      });
-                    },
-                    child: Text('Light'),
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsets.only(top: 50, left: 3),
-                  child: ElevatedButton(
-                    style: ButtonStyle(
-                      backgroundColor: MaterialStateProperty.all<Color>(Colors.black),
-                      foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
-                    ),
-                    onPressed: () {
-                      setState(() {
-                        darkMode = true;
-                      });
-                    },
-                    child: Text('Dark'),
-                  ),
-                ),
-              ],
-            ),
+            )
           ],
         ),
       ),
